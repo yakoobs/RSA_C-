4f895c2bc5f4a0684c828f83e421ef5510268808
Jakub Sokołowski <jakub.sokolowski@partners.mbank.pl>
2016-03-15 01:53:55 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+
+
+//Encoding
+//1. Read a line from file text.txt
+//2. Encode the line
+//3. Write encoded line into encoded_text.txt
+
+//Decoding
+//1. Read encoded line from file encoded_text.txt
+//2. Decode the line
+//3. Write decoded line to file decoded_text.txt
+
+//Test
+//1. Read the A line from file text.txt
+//2. Read the B line from file decoded_text.txt
+//3. Compare these lines
+//4. If all lines are equal test passes.
