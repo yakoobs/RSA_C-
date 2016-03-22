9f19ab538bc09b97db11de3a729adb6b1ff66ece
Jakub Sokołowski <jakub.sokolowski@partners.mbank.pl>
2016-03-22 01:10:35 +0100

    Add initial implementation

---
+++ Constants.h
@@ -0,0 +1,16 @@
+//
+// Created by Jakub Sokołowski on 3/15/16.
+//
+
+#ifndef RSA_CONSTANTS_H
+#define RSA_CONSTANTS_H
+
+#define PUBLIC_KEY_e  11
+#define PUBLIC_KEY_n  899
+
+#define PRIVATE_KEY_d  611
+#define PRIVATE_KEY_n  PUBLIC_KEY_n
+
+#define CONVERSION_CONST 96
+
+#endif //RSA_CONSTANTS_H
--- main.c
+++ main.c
@@ -1,17 +1,93 @@
+//// Created by Jakub Sokołowski on 3/15/16.
+////
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Constants.h"
+
+long int encoded[1000];
+char decoded[1000];
+void decryptText();
+void testEncryption();
+int decryptCharacter(int c);
+int encryptCharacter(int l);
+
+long long int RSA_modulo(int b, int d, int n);
+long long int square(long long a);
+
+int convertCharacterToInt(char c);
+char convertIntToCharacter(int l);
+
+int main() {
+    testEncryption();
+
+    return EXIT_SUCCESS;
+}
+
+#define EXAMPLE_TEXT "testowy tekst testowy tekst testowy tekst testowy tekst testowy tekst "
+
+void testEncryption() {
+
+    const char* text = EXAMPLE_TEXT;
+    size_t len = strlen(text);
+
+    for(int i = 0; i < len; i++) {
+        encoded[i] = encryptCharacter(convertCharacterToInt(text[i]));
+        printf(" %ld",encoded[i]) ;
+    }
+
+    decryptText();
+    printf("\nDECODED: %s ",decoded) ;
+
+    int isCorrectlyDecoded = (strcmp(text, decoded) == 0);
+    if (isCorrectlyDecoded) {
+        printf("\n Text has been decoeded CORRECTLY");
+    } else {
+        printf("\n Text has been decoded INCORRECTLY");
+    }
+}
+
+void decryptText() {
+    size_t len = strlen(EXAMPLE_TEXT);
+
+    for(int i = 0; i < len; i++) {
+        decoded[i] = convertIntToCharacter(decryptCharacter(encoded[i]));
+    }
+    decoded[len] = '\0';
+}
 
 
-//Encoding
-//1. Read a line from file text.txt
-//2. Encode the line
-//3. Write encoded line into encoded_text.txt
+//Encryption and decryption of characters
+int encryptCharacter(int l) {
+    return RSA_modulo(l, PUBLIC_KEY_e, PUBLIC_KEY_n);
+}
 
-//Decoding
-//1. Read encoded line from file encoded_text.txt
-//2. Decode the line
-//3. Write decoded line to file decoded_text.txt
+int decryptCharacter(int c) {
+    return RSA_modulo(c, PRIVATE_KEY_d, PRIVATE_KEY_n);
+}
 
-//Test
-//1. Read the A line from file text.txt
-//2. Read the B line from file decoded_text.txt
-//3. Compare these lines
-//4. If all lines are equal test passes.
+
+//Calculating
+long long int square(long long arg) { return arg * arg; }
+
+long long int RSA_modulo(int b, int d, int n) {
+    if(d == 0) {
+        return 1;
+    } else if(d % 2 == 0) {
+        return square(RSA_modulo(b, d / 2, n)) % n;
+    } else {
+        return ((b % n) * RSA_modulo(b, d - 1, n)) % n;
+    }
+}
+
+//Initial conversion
+int convertCharacterToInt(char c) {
+    if (c == ' ') return 0;
+    return (int)(c - CONVERSION_CONST);
+}
+
+char convertIntToCharacter(int l) {
+    if (l == 0) return ' ';
+    return (char) (l + CONVERSION_CONST);
+}
