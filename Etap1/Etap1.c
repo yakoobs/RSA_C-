1b607312bfab53c4885b3e1f436e1e1e0491ed24
Kuba Sokolowski <jakub.m.sokolowski@gmail.com>
2016-03-30 02:06:23 +0200

    Load and save encoded text methods

--- Etap1/Etap1.c
+++ Etap1/Etap1.c
@@ -8,17 +8,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_TEXT_LENGTH 2048
-#define TEXT_FILE_PATH "/Users/kuba.sokolowski/Desktop/text.txt"
-#define ENCODED_TEXT_FILE_PATH "/Users/kuba.sokolowski/Desktop/encodedText.txt"
-#define DECODED_TEXT_FILE_PATH "/Users/kuba.sokolowski/Desktop/decodedText.txt"
 
 #define NUMBER_OF_PROCESSORS 2
 
 char* textBuffer;
 long textLength;
-long int encodedText[MAX_TEXT_LENGTH];
+long int encoded[MAX_TEXT_LENGTH];
+char* encodedText;
 char* decodedText;
+
+void writeToFile(char* filePath, char* text);
+
 
 void loadTextFromFile() {
     FILE *fp = fopen(TEXT_FILE_PATH, "r");
@@ -29,7 +29,7 @@ void loadTextFromFile() {
     rewind( fp );
 
     textBuffer = calloc( 1, textLength+1 );
-    //encodedText = calloc( 1, textLength+1 );
+    encodedText = calloc( 1, textLength+1 );
     decodedText = calloc( 1, textLength+1 );
 
     if( !textBuffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
@@ -41,20 +41,37 @@ void loadTextFromFile() {
 }
 
 void saveEncodedTextToFile() {
-//    FILE *fp = fopen(ENCODED_TEXT_FILE_PATH, "w");
-//
-//    int results = fputs(encodedText, fp);
-//    if (results == EOF) {
-//        fputs("No text has been written",stderr),exit(1);
-//    }
-//    fclose(fp);
+    FILE *fp = fopen(ENCODED_TEXT_FILE_PATH, "w");
+    for (int i = 0; i < textLength; ++i) {
+        fprintf(fp,"%li\n",encoded[i]);
+    }
+    fclose(fp);
+}
+
+void loadEncodedTextFromFile() {
+    char * line = NULL;
+    size_t len = 0;
+    int index = 0;
+
+    FILE *fp = fopen(ENCODED_TEXT_FILE_PATH, "r");
+    if( !fp ) perror(ENCODED_TEXT_FILE_PATH),exit(1);
+
+    while (getline(&line, &len, fp) < 0) {
+        encoded[index++] = atoi(line);
+    }
+
+    fclose(fp);
 }
 
 
 void saveDecodedTextToFile() {
-    FILE *fp = fopen(DECODED_TEXT_FILE_PATH, "w");
+    writeToFile(DECODED_TEXT_FILE_PATH,decodedText);
+}
 
-    int results = fputs(decodedText, fp);
+void writeToFile(char* filePath, char* text) {
+    FILE *fp = fopen(filePath, "w");
+
+    int results = fputs(text, fp);
     if (results == EOF) {
         fputs("No text has been written",stderr),exit(1);
     }
@@ -64,21 +81,15 @@ void saveDecodedTextToFile() {
 void testEncryption() {
 
     loadTextFromFile();
-    const char* text = textBuffer;
-    size_t len = strlen(text);
+    encryptText();
 
-    for(int i = 0; i < len; i++) {
-        encodedText[i] = encryptCharacter(convertCharacterToInt(text[i]));
-        printf("%ld ",encodedText[i]) ;
-    }
-
-   // saveEncodedTextToFile();
+    saveEncodedTextToFile();
     decryptText();
-   // saveDecodedTextToFile();
+    saveDecodedTextToFile();
 
     printf("\nDECODED: %s", decodedText) ;
 
-    int isCorrectlyDecoded = (strcmp(text, decodedText) == 0);
+    int isCorrectlyDecoded = (strcmp(textBuffer, decodedText) == 0);
     if (isCorrectlyDecoded) {
         printf("\nText has been decoded CORRECTLY");
     } else {
@@ -86,10 +97,20 @@ void testEncryption() {
     }
 }
 
+void encryptText() {
+    const char* text = textBuffer;
+    size_t len = strlen(text);
+
+    for(int i = 0; i < len; i++) {
+        encoded[i] = encryptCharacter(convertCharacterToInt(text[i]));
+        printf("%ld ", encoded[i]) ;
+    }
+}
+
 void decryptText() {
     size_t len = textLength;
 
     for(int i = 0; i < len; i++) {
-        decodedText[i] = convertIntToCharacter(decryptCharacter(encodedText[i]));
+        decodedText[i] = convertIntToCharacter(decryptCharacter(encoded[i]));
     }
 }
--- Etap1/Etap1.h
+++ Etap1/Etap1.h
@@ -5,8 +5,15 @@
 #ifndef RSA_TESTETAPI_H
 #define RSA_TESTETAPI_H
 
+void encryptText();
 void decryptText();
 void testEncryption();
+
+#define MAX_TEXT_LENGTH 2048
+#define TEXT_FILE_PATH "/Users/kuba.sokolowski/Desktop/text.txt"
+#define ENCODED_TEXT_FILE_PATH "/Users/kuba.sokolowski/Desktop/encodedText.txt"
+#define DECODED_TEXT_FILE_PATH "/Users/kuba.sokolowski/Desktop/decodedText.txt"
+
 
 /**
  * Writing and reading from the text file
